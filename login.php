875ef04668919373ab2c041c185fcccd8ae1e4d5
Alvaro <alvaro.serrano@wmich.edu>
2019-04-03 13:28:26 -0400

    first

---
+++ login.php
@@ -0,0 +1,36 @@
+<html>  
+    <head>  
+        <title>Chat Application using PHP Ajax Jquery</title>  
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+        <br />
+        
+        <h3 align="center">Chat Application using PHP Ajax Jquery</a></h3><br />
+        <br />
+        <div class="panel panel-default">
+            <div class="panel-heading">Chat Application Login</div>
+            <div class="panel-body">
+            <form method="post" action = "login.inc.php">
+            <p class="text-danger"><?php echo $message; ?></p>
+            <div class="form-group">
+            <label>Enter Username</label>
+            <input type="text" name="username" class="form-control" required />
+            </div>
+            <div class="form-group">
+            <label>Enter Password</label>
+            <input type="password" name="password" class="form-control" required />
+            </div>
+            <div class="form-group">
+            <input type="submit" name="login" class="btn btn-info" value="Login" />
+            </div>
+            </form>
+            </div>
+        </div>
+        </div>
+    </body>  
+</html>
